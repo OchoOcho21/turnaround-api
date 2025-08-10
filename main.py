16273dff11648408709fdbae04f04d98aec1b072
OchoOcho21 <ochoocho21162@gmail.com>
2025-08-10 17:36:39 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
-import playwright.sync_api
+import asyncio
 import requests
 import flask
 import json
 import time
 import os
+import pyppeteer
 
-def setup_solver(): #please body dont sue me
-    if not os.path.exists("utils"): os.mkdir("utils")
-    files = ["https://raw.githubusercontent.com/Body-Alhoha/turnaround/main/utils/solver.py", "https://raw.githubusercontent.com/Body-Alhoha/turnaround/main/utils/page.html"]
+def setup_solver():
+    if not os.path.exists("utils"): 
+        os.mkdir("utils")
+    files = [
+        "https://raw.githubusercontent.com/OchoOcho21/ocho-turnstile-solver/main/utils/solver.py",
+        "https://raw.githubusercontent.com/OchoOcho21/ocho-turnstile-solver/main/utils/page.html"
+    ]
     for file in files:
         r = requests.get(file).text
         with open("utils/" + file.split("/")[-1], "w") as f:
@@ -15,27 +20,33 @@ def setup_solver(): #please body dont sue me
 
 setup_solver()
 app = flask.Flask(__name__)
-from utils import solver
+from utils.solver import Solver
+
+async def solve_captcha(url, sitekey, invisible, proxy=None):
+    solver = Solver(proxy=proxy, headless=True)
+    try:
+        start_time = time.time()
+        print(f'Solving captcha with proxy: {proxy}')
+        token = await solver.solve(url, sitekey, invisible)
+        print(f"took {time.time() - start_time} seconds :: {token[:10]}")
+        return token
+    finally:
+        await solver.terminate()
 
 @app.route("/")
 def index():
-    return flask.redirect("https://github.com/Euro-pol/turnaround-api")
+    return flask.redirect("https://github.com/OchoOcho21/turnaround-api")
 
 @app.route("/solve", methods=["POST"])
-def solve():
+async def solve():
     json_data = flask.request.json
     sitekey = json_data["sitekey"]
     invisible = json_data["invisible"]
     url = json_data["url"]
     proxy = json_data.get('proxy')
-    with playwright.sync_api.sync_playwright() as p:
-        s = solver.Solver(p, proxy = proxy, headless=True)
-        start_time = time.time()
-        print('Solving captcha with proxy: ' + str(proxy))
-        token = s.solve(url, sitekey, invisible)
-        print(f"took {time.time() - start_time} seconds :: " + token[:10])
-        s.terminate()
-        return make_response(token)
+    
+    token = await solve_captcha(url, sitekey, invisible, proxy)
+    return make_response(token)
 
 def make_response(captcha_key):
     if captcha_key == "failed":
@@ -43,4 +54,5 @@ def make_response(captcha_key):
     return flask.jsonify({"status": "success", "token": captcha_key})
 
 if __name__ == "__main__":
-    app.run()
+    port = 12019
+    app.run(host='0.0.0.0', port=port)
